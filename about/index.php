c9c041a0be733dec344078d1653027b60e56439d
Reece Trolley <reecetrolley@gmail.com>
2019-03-20 17:16:06 +0000

    Add about us page

---
+++ about/index.php
@@ -0,0 +1,22 @@
+<html lang="">
+<head>
+    <?php include('/var/www/html/assets/dependencies.php'); ?>
+    <?php include('/var/www/html/assets/php_functions.php'); ?>
+    <title>Home</title>
+</head>
+<body>
+<!-- Navbar section -->
+<?php
+make_navbar("Home")
+?>
+
+<!-- Main Content -->
+<h1>
+    Index page!
+</h1>
+
+<script>
+</script>
+
+</body>
+</html>
