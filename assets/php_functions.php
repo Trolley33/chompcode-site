7c9881f960de71ff687d6a5c248df04c564e783d
Reece Trolley <reecetrolley@gmail.com>
2019-03-22 00:03:11 +0000

    Change text to image

--- assets/php_functions.php
+++ assets/php_functions.php
@@ -13,7 +13,9 @@ function make_navbar($active)
     // Start open navbar tags.
     echo '
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">ChompCode</a>
+    <a class="navbar-brand" href="/">
+        <img src="/assets/chompcode-small-circle.php" width="30" height="30" alt="" />
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
