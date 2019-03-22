e248ef16031ba2bf8b5411a5f3573fb7e36417ef
Reece Trolley <reecetrolley@gmail.com>
2019-03-22 00:43:48 +0000

    Make dummy project post

--- projects/index.php
+++ projects/index.php
@@ -8,13 +8,24 @@
 <body>
 <!-- Navbar section -->
 <?php
-make_navbar("Projects")
+make_navbar("Projects");
 ?>
 <!-- Main Content -->
 <div class="container">
-    <h1>
-        Project page!
-    </h1>
+    <div class="row">
+        <div class="col-md-8">
+            <h1 class="my-4">Our Projects</h1>
+    <div class="card mb-4" style="width: 18rem;">
+       <img src="https://i.imgur.com/WkkdraP.png" width='100%' height='180' class="card-img-top" alt="..">
+       <div class="card-body">
+           <h5 class="card-title">Chompcode.com</h5>
+           <p class="card-text">This project is what you're seeing right now! This website was made from scratch by us here at ChompCode. If you want to find out how we did everything, then this is the article for you!</p>
+           <a href="#" class="btn btn-primary">View More</a>
+       </div>
+       <div class="card-footer text-muted">
+           Posted on March 22nd, 2019
+       </div>
+    </div>
 </div>
 
 <script>
